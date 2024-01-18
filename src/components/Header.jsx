59be073127e02383c548b04fa27864955c5bb0a8
dduares <156270568+dduares@users.noreply.github.com>
2024-01-18 09:51:26 +0900

    6_header_완성

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const headerNav=[
   {
@@ -25,13 +25,20 @@ const headerNav=[
 
 
 const Header = () => {
+
+  const [show,setShow] = useState(false);
+  const toggleMenu=()=>{
+    setShow((prevShow)=>!prevShow)
+  }
+
+
   return (
     <header id='header'>
       <div className="header_inner">
         <div className="header_logo">
-          <a href="#">portofolio <em>react</em></a>
+          <a href="/">portofolio <em>react</em></a>
         </div>
-        <nav className='header_nav' aria-label='메인메뉴'>
+        <nav className={`header_nav ${show? "show":""}`} aria-label='메인메뉴'>
           <ul>
             {headerNav.map((nav,key)=>(
               <li key={key}>
@@ -43,10 +50,10 @@ const Header = () => {
         <div className="header_nav_mobile"
           id='headerToggle'
           aria-controls='primary-menu'
-          aria-expanded='false'
+          aria-expanded={show?"true":"false"}
           role='button'
           tabIndex='0'
-        >
+          onClick={toggleMenu}>
           <span></span>
         </div>
       </div>
